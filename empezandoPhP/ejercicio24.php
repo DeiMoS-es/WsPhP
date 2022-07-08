1d0b52c96927e3ff25329c3629801a6b633480d6
Nagib Delgado Morales <ndelgado@hiberus.com>
2022-07-08 13:53:22 +0200

    ambito

--- empezandoPhP/ejercicio24.php
+++ empezandoPhP/ejercicio24.php
@@ -3,6 +3,8 @@
     class persona{
 
         public $nombre; //Propiedades
+        private $edad;//solo se puede acceder desde la clase
+        protected $altura; //solo se puede acceder desde la misma clase, o clases heredadas
 
         //Método, acciones
         public function asignarNombre($nuevoNombre){
@@ -12,12 +14,18 @@
         public function imprimirNombre(){
             echo "Hola soy: ".$this->nombre;
         }
+
+        public function mostrarEdad(){
+            $this->edad = 20;
+            return $this->edad;
+        }
     }
 
     //Creo el objeto alumno a partir de la clase
     $objAlumno = new persona();
     $objAlumno->asignarNombre("Nagib");//llamo a un método
     echo $objAlumno->nombre;//imprimo una propiedad
+    echo "<br>".$objAlumno->mostrarEdad()."<br>";
 
     $objAlumno2 = new persona();
     $objAlumno2->asignarNombre("Celia");
