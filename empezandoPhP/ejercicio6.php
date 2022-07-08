1733722b334ea9861f938c7ee14122f5f3f155a9
Nagib Delgado Morales <ndelgado@hiberus.com>
2022-07-08 10:43:46 +0200

    tipos de datos

---
+++ empezandoPhP/ejercicio6.php
@@ -0,0 +1,9 @@
+<?php
+    // con $ se declara la variable, y al inicializarla (darle un valor inicial) se declara el tipo de dato
+    $Edad = 34; //Entero
+    $Nombre = "Nagib";//String
+    $Altura = 1.4;//Float
+    $Programador = null;//Valor nulo
+
+    echo "El nombre es: ".$Nombre." la edad es: ".$Edad." la altura es: ".$Altura." el puesto es: ".$Programador; 
+?>
