98de0a0b9ae9ff0e511bc6f21332feffab28e0c9
Nagib Delgado Morales <ndelgado@hiberus.com>
2022-07-08 13:31:39 +0200

    recorriendo Arrays

--- empezandoPhP/ejercicio21.php
+++ empezandoPhP/ejercicio21.php
@@ -1,6 +1,10 @@
 <?php
     $frutas = array("Fresa", "Pera", "Manzana");
 
-    print_r($frutas);
+    //print_r($frutas);
+    
+    for($i=0; $i<3; $i++){
+        echo $frutas[$i]."<br>";
+    }
 
 ?>
--- empezandoPhP/ejercicio22.php
+++ empezandoPhP/ejercicio22.php
@@ -1,6 +1,11 @@
 <?php
 //Cambiando el índice
     $frutas = array("f"=>"Fresa", "p"=>"Pera", "m"=>"Manzana");
-    print_r($frutas);
+   // print_r($frutas);
+   foreach($frutas as $i=>$valor){
+   // echo $i."<br>";
+   // echo $valor."<br>";
+    echo $frutas[$i]."<br>";
+   }
 
 ?>
