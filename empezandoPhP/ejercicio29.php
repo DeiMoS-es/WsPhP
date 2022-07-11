b52093c39667766d9d773d52e47e9f55c6ff35d0
Nagib Delgado Morales <ndelgado@hiberus.com>
2022-07-11 08:40:56 +0200

    lectura ejercicio 29

--- empezandoPhP/ejercicio29.php
+++ empezandoPhP/ejercicio29.php
@@ -10,15 +10,17 @@
         $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
         $sql = "SELECT * FROM `fotos`";
-        $sentencia = $conexion->prepare($sql);
-        $sentencia->execute();
+        $sentencia = $conexion->prepare($sql);//ejecuta la instrucción anterior, y se almacena en la variable sentencia
+        $sentencia->execute();//con execute ejecutamos esa sentencia de obtener los datos
 
-        $resultado = $sentencia->fetchAll();
+        $resultado = $sentencia->fetchAll();//los resultados se almacenaran en la variable resultado, fetcAll devuelve todo el resultado de la sentencia
 
         //print_r($resultado);
 
         foreach($resultado as $foto){//$foto o como me de la gana
-            print_r($foto['nombre']);
+            //print_r($foto['nombre']);
+            //otra forma de mostrar los datos
+            echo $foto['nombre']."<br>";
         }
 
         echo "Conexion establecida";
