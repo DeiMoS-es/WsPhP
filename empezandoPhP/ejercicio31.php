0f64401f9fc4dfa8d4c93652b45cd2b0b077529c
DeiMoS <nagib.86@gmail.com>
2022-07-12 17:57:35 +0200

    envio radio button

--- empezandoPhP/ejercicio31.php
+++ empezandoPhP/ejercicio31.php
@@ -1,9 +1,12 @@
 <?php
 
 $txtNombre = "";
+$rdgLenguaje = "";
 if($_POST){//si hay envio
     $txtNombre = (isset($_POST['txtNombre'])) ?$_POST['txtNombre']:"" ;//pregunto si el dato ha llegado, si hay le asignas el nobre, si no hay dato asigna cadena vacía
+    $rdgLenguaje = (isset($_POST['lenguaje'])) ?$_POST['lenguaje']:"" ;
 
+    print_r($rdgLenguaje);
 }
 
 ?>
@@ -19,10 +22,17 @@ if($_POST){//si hay envio
 <body>
     <?php if($_POST){ ?><!-- mediante código embevido pregunto si hay envio para mostrar el saludo -->
     <strong> Hola </strong> : <?php echo $txtNombre; ?>
+    <br>
+    <strong>Tu lenguaje es: </strong> <?php echo $rdgLenguaje; ?>
     <?php } ?>
     <form action="ejercicio31.php" method="post">
+        <p>Nombre:</p>        
         <!-- Le digo que me guarde el valor del nombre que introduzca -->
         <input value="<?php echo $txtNombre; ?>" type="text" name="txtNombre">
+        <p>¿Te gusta?:</p>
+        <br> php: <input type="radio" <?php echo ($rdgLenguaje=="php")?"checked":""; ?> name="lenguaje" value="php"> <br>
+        <br> html:<input type="radio" <?php echo ($rdgLenguaje=="html")?"checked":""; ?> name="lenguaje" value="html"> <br>
+        <br> css: <input type="radio" <?php echo ($rdgLenguaje=="css")?"checked":""; ?> name="lenguaje" value="css"> <br>
         <button type="submit">Enviar</button>        
     </form>
 </body>
